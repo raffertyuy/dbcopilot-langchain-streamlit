7afd345fed4eabdd169a30ce193bc7e740a6d3bb
Rafferty Uy <rafferty.uy@gmail.com>
2023-10-05 11:21:35 +0800

    minor updates

--- app/pages/2_👨‍💻_DataFrame_Agent.py
+++ app/pages/2_👨‍💻_DataFrame_Agent.py
@@ -17,7 +17,7 @@ st.title("👨‍💻 Chat with your Data using SQL Stored Procedures and the La
 st.write("Query your data using natural language and output the results as a table, bar chart, line chart, or a plain answer.")
 st.caption("This app is using the [Northwind Traders](https://github.com/raffertyuy/RazGPT/tree/main/sampledata) sample database.")
 
-st.header("**_🚧 NOTE: This feature is still under development. 🚧_**")
+st.markdown("> **_🚧 NOTE: This feature is still under development. 🚧_**")
     
 st.subheader("Step 1: Retrieve Data")
 st.selectbox('Which data do you want to retrieve?', ('Customers', 'Orders', 'Products'))
